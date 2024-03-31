f8a54f9fec25d353273d726ed695cad582c40cbb
Rivaldi <cbot59@gmail.com>
2024-03-31 10:05:23 +0800

    feat: initial commit

---
+++ src/main/java/dev/rivaldi/springbooth2file/SpringBootH2FileApplication.java
@@ -0,0 +1,13 @@
+package dev.rivaldi.springbooth2file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2FileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootH2FileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/rivaldi/springbooth2file/SpringBootH2FileApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rivaldi.springbooth2file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootH2FileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
